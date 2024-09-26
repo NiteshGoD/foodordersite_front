518a9976e833a9f622d9081e68f1dea6bed08793
NiteshGoD <nit.ranjit@hotmail.com>
2024-09-26 10:04:48 +0545

    Frontend

---
+++ src/AppRoutes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Home/HomePage'
+import FoodPage from './pages/Food/FoodPage'
+import CartPage from './pages/Cart/CartPage'
+import LoginPage from './pages/Login/LoginPage'
+
+export default function AppRoutes() {
+  return (<Routes>
+    <Route path = "/" element = {<HomePage />}></Route>
+    <Route path="/search/:searchTerm" element={<HomePage />}></Route>
+    <Route path="/tag/:tag" element={<HomePage />}></Route>
+    <Route path="/food/:id" element = {<FoodPage />}></Route>
+    <Route path="/cart" element = {<CartPage />}></Route>
+    <Route path="/login" element = {<LoginPage />}></Route>
+  </Routes>
+  )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import classes from './header.module.css';
+import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/useCart';
+import { useAuth } from '../../hooks/useAuth';
+
+export default function Header() {
+    const {user, logout } = useAuth();
+    const {cart} = useCart();
+    // const logout = () => {
+    //     console.log("Logging out!!!")
+    // }
+  return (
+   <header className={classes.header}>
+    <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+            Food Mine!
+        </Link>
+        <nav>
+            <ul>
+                {
+                    user?(
+                    <li className={classes.menu_container}>
+                        <Link to="/profile">{user.name}</Link>
+                        <div className={classes.menu}>
+                            <Link to="/profile">Profile</Link>
+                            <Link to="/orders">Orders</Link>
+                            <a onClick={logout}>Logout</a>
+                            {/* <Navigate to="/logout"><a>logout</a></Navigate> */}
+                        </div>
+                    </li> ) : (
+                    <Link to="/login">Login</Link>
+                )}
+                <li>
+                    <Link to="/cart">
+                        Cart
+                        {cart.totalCount > 0 && <span className={classes.cart_count}>{cart.totalCount}</span>}
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+   </header>
+  )
+}
